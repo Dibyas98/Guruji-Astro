3076978506f656ea5fb829db8884e8e058c926ad
Dibyas98 <dibyashaktip98@gmail.com>
2024-05-31 23:35:21 +0530

    error handel

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Notfound() {
+  return (
+    <div className='back w-full h-screen  flex flex-col items-center gap-5 justify-center'>
+        <h1 className='text-6xl text-center'>404 Page Not Found</h1>
+        <Link href={'/'}>
+            <button className='button-5'>Return to Home Page</button>
+        </Link>
+    </div>
+  )
+}
+
+export default Notfound
