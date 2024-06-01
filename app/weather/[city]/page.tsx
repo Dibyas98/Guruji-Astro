16cc1535385e3b10485f18d96046b359750f0dbd
Dibyas98 <dibyashaktip98@gmail.com>
2024-06-01 11:13:50 +0530

    m

--- app/weather/[city]/page.tsx
+++ app/weather/[city]/page.tsx
@@ -6,11 +6,8 @@ import { useParams } from 'next/navigation';
 import React, { useContext, useEffect } from 'react'
 
 function page() {
-  const {HadelParams,currentWeather,load} = useContext(GlobalContext)
+  const {currentWeather,load} = useContext(GlobalContext)
   const params = useParams()
-  useEffect(()=>{
-    HadelParams(params.city)
-  },[])
   return (
     <div className="p-2 md:p-5 xl:p-5  bg-[#c7d1cda3] xl:h-screen">
       {/* DISPALY LOADER  */}
